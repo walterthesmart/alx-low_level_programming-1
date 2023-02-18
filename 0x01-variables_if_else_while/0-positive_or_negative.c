84ddb9194e208ead0610d6ee3c0eecb42f9c92f3
ELIZA-BETH002 <106850645+ELIZA-BETH002@users.noreply.github.com>
2023-02-18 11:44:45 +0100

    new commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,17 +8,17 @@
  */
 int main(void)
 {
-README.md int n;
+	int n;
 
-README.md srand(time(0));
-README.md n = rand() - RAND_MAX / 2;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-README.md if (n > 0)
-README.md README.md printf("%d is positive\n", n);
-README.md else if (n == 0)
-README.md README.md printf("%d is zero\n", n);
-README.md else
-README.md README.md printf("%d is negative\n", n);
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
 
-README.md return (0);
-} 
+	return (0);
+}
