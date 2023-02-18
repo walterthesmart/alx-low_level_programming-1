677fee76de422b0d7f42ae9855e7d77114e90bc3
ELIZA-BETH002 <106850645+ELIZA-BETH002@users.noreply.github.com>
2023-02-18 13:30:29 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
-#!/bin/bash
- #include <stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -8,6 +7,6 @@
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-} 
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
