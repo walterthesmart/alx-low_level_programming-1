9204be36907245d8cfd474f522f5d7874f09690e
ELIZA-BETH002 <106850645+ELIZA-BETH002@users.noreply.github.com>
2023-03-08 22:15:22 +0100

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,4 @@
- #include "main.h"
+#include "main.h"
 /**
  * _puts_recursion - print a string
  * @s:string
@@ -8,12 +8,12 @@ void _puts_recursion(char *s)
 {
 if (*s)
 {
-_putchar(*s);
-_puts_recursion(s + 1);
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
 else
 {
-_putchar(n);
+	_putchar('\n');
 }
 
 }
