55bd3f6b4cd957829ef2439e0dedc894dc85cbe6
ELIZA-BETH002 <elizabethnwaugo@gmail.com>
2023-03-10 09:26:47 -0800

    argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+ #include <stdio.h>
+
+/**
+ * main - prints its name, followed by a new line.
+ * @argc: argument count
+ * @argv: arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+README.md (void)argc;
+
+README.md printf("%s\n", argv[0]);
+README.md return (0);
+} 
