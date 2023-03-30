7683e936d3e2a03a95bec644eaf74bd54898fd32
ELIZA-BETH002 <106850645+ELIZA-BETH002@users.noreply.github.com>
2023-03-30 20:57:45 +0100

    Update 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -10,11 +10,11 @@
  */
 unsigned int _strlen(char *str)
 {
-unsigned int i;
+	unsigned int i;
 
-for (i = 0; str[i]; i++)
-;
-return (i);
+	for (i = 0; str[i]; i++)
+		;
+	return (i);
 }
 
 /**
@@ -26,29 +26,29 @@ return (i);
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-list_t *new, *tmp;
+	list_t *new, *tmp;
 
-if (str == NULL)
-return (NULL);
-new = malloc(sizeof(list_t));
-if (new == NULL)
-return (NULL);
-new->str = strdup(str);
-if (new->str == NULL)
-{
-free(new);
-return (NULL);
-}
-new->len = _strlen(new->str);
-new->next = NULL;
-if (*head == NULL)
-{
-*head = new;
-return (new);
-}
-tmp = *head;
-while (tmp->next)
-tmp = tmp->next;
-tmp->next = new;
-return (new);
+	if (str == NULL)
+		return (NULL);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->len = _strlen(new->str);
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	tmp = *head;
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = new;
+	return (new);
 }
