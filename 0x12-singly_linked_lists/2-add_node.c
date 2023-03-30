575b631db7917e9c58ef4573799900c78b5c6944
ELIZA-BETH002 <106850645+ELIZA-BETH002@users.noreply.github.com>
2023-03-30 20:56:49 +0100

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,13 +10,13 @@
  */
 int _strlen(const char *s)
 {
-int i = 0;
+	int i = 0;
 
-while (s[i] != 0)
-{
-i++;
-}
-return (i);
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
 
 /**
@@ -27,16 +27,16 @@ return (i);
  */
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *add;
+	list_t *add;
 
-add = malloc(sizeof(list_t));
-if (add == NULL)
-return (NULL);
-add->str = strdup(str);
+	add = malloc(sizeof(list_t));
+	if (add == NULL)
+		return (NULL);
+	add->str = strdup(str);
 
-add->len = _strlen(str);
-add->next = *head;
-*head = add;
+	add->len = _strlen(str);
+	add->next = *head;
+	*head = add;
 
-return (add);
+	return (add);
 }
