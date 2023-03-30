1e75c70ca6a6faaab295b949c1940f5cfa321a12
ELIZA-BETH002 <106850645+ELIZA-BETH002@users.noreply.github.com>
2023-03-30 21:19:51 +0100

    Update 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,21 +1,21 @@
-externprintf; the C function, to be called
+extern	printf		; the C function, to be called
 
-        section .data; Data section, initialized variables
-msg:db "Hello, Holberton", 0 ; C string needs 0
-fmt:    db "%s", 10, 0          ; The printf format, "\n",0
+	        section .data		; Data section, initialized variables
+msg:		db "Hello, Holberton", 0 ; C string needs 0
+fmt:	    	db "%s", 10, 0          ; The printf format, "\n",'0'
 
-        section .text; Code section.
+	        section .text		; Code section.
 
-        global main; the standard gcc entry point
-main:; the program label for the entry point
-        push    rbp; set up stack frame, must be alligned
+	        global main		; the standard gcc entry point
+main:					; the program label for the entry point
+	        push    rbp		; set up stack frame, must be alligned
 
-movrdi,fmt
-movrsi,msg
-movrax,0; or can be  xor  rax,rax
-        call    printf; Call C function
+		mov	rdi,fmt
+		mov	rsi,msg
+		mov	rax,0		; or can be  xor  rax,rax
+	        call    printf		; Call C function
 
-poprbp; restore stack
+		pop	rbp		; restore stack
 
-movrax,0; normal, no error, return value
-ret; return
+		mov	rax,0		; normal, no error, return value
+		ret			; return
