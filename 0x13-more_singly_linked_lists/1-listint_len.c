f3458d11a30622885899aa898c06de04e237b061
ELIZA-BETH002 <106850645+ELIZA-BETH002@users.noreply.github.com>
2023-04-04 12:23:11 +0100

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -11,12 +11,12 @@
 
 size_t listint_len(const listint_t *h)
 {
-size_t count = 0;
+	size_t count = 0;
 
-while (h != NULL)
-{
-h = h->next;
-count++;
-}
-return (count);
+	while (h != NULL)
+	{
+	h = h->next;
+		count++;
+	}
+	return (count);
 }
