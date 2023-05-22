74a94c41c8a8f797576beeb2603b8f9f076019bc
ELIZA-BETH002 <elizabethnwaugo@gmail.com>
2023-05-22 14:25:37 -0700

    libraries

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,12 @@
+#include "mylib.h"
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int subtract(int a, int b)
+{
+    return a - b;
+}
+
---
+++ 0x18-dynamic_libraries/mylib.h
@@ -0,0 +1,8 @@
+#ifndef MYLIB_H
+#define MYLIB_H
+
+int add(int a, int b);
+int subtract(int a, int b);
+
+#endif /* MYLIB_H */
+
