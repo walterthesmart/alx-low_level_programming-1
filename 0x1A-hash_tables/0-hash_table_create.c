b4812504a77c2db1209684d26cc94030d0de21a6
ELIZA-BETH002 <106850645+ELIZA-BETH002@users.noreply.github.com>
2023-06-09 21:02:07 +0100

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,30 +1,47 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - Created hash table
- * @size: The size of the array
+
+ * hash_table_create - creates a hash table
+
+ * @size: size of the array
+
  *
- * Return: A pointer to the hash table
+
+ * Return: pointer to the newly created hash table
+
  */
+
 hash_table_t *hash_table_create(unsigned long int size)
+
 {
-hash_table_t *table;
-unsigned long int i = 0;
 
-if (size == 0)
-return (NULL);
+	hash_table_t *hash_table;	unsigned long int i;
 
-table = malloc(sizeof(hash_table_t));
-if (table == NULL)
-return (NULL);
+	hash_table = malloc(sizeof(hash_table_t));
 
-table->array = malloc(sizeof(hash_node_t *) * size);
-if (table->array == NULL)
-return (NULL);
+	if (hash_table == NULL)
 
-for (i = 0; i < size; i++)
-table->array[i] = NULL;
+		return (NULL);
 
-table->size = size;
-return (table);
+	hash_table->size = size;
+
+	hash_table->array = malloc(size * sizeof(hash_node_t *));
+
+	if (hash_table->array == NULL)
+
+	{
+
+		free(hash_table);
+
+		return (NULL);
+
+	}
+
+	for (i = 0; i < size; i++)
+
+		hash_table->array[i] = NULL;
+
+	return (hash_table);
+
 }
